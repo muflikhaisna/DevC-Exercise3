05efa5545036d92300bf32a9012f58ba616bc172
muflikhaisna <muflikhahisna0@gmail.coom>
2019-08-30 10:11:29 +0700

    add image

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
 function requestJSON(nama = "") {
 	fetch("https://swapi.co/api/people/?format=json&page=1")
-		.then(Response => {
-			return Response.json();
+		.then(resnponse => {
+			return resnponse.json();
 		})
 		.then(data => {
 			var arrayJSON = data.results;
 			console.log(arrayJSON);
+			var konten =
+				'<thead><tr class="bg-warning"><th scope="col">Name</th><th scope="col">Height</th><th scope="col">Weight</th><th scope="col">Gender</th></tr></thead>';
+			for (var i = 0; i < arrayJSON.length; i++) {
+				console.log(i);
+				console.log(arrayJSON[i].name);
+				if (nama !== "") {
+					if (arrayJSON[i].name.includes(nama)) {
+						konten +=
+							'<tr class="table-warning"><td>' +
+							arrayJSON[i].name +
+							"</td>" +
+							"<td>" +
+							arrayJSON[i].height +
+							"</td>" +
+							"<td>" +
+							arrayJSON[i].mass +
+							"</td>" +
+							"<td>" +
+							arrayJSON[i].gender +
+							"</td>" +
+							"</tr>";
+					}
+				} else {
+					konten +=
+						'<tr class="table-warning"><td>' +
+						arrayJSON[i].name +
+						"</td>" +
+						"<td>" +
+						arrayJSON[i].height +
+						"</td>" +
+						"<td>" +
+						arrayJSON[i].mass +
+						"</td>" +
+						"<td>" +
+						arrayJSON[i].gender +
+						"</td>" +
+						"</tr>";
+				}
+			}
 
-			var konten = "";
+			console.log(konten);
+
+			document.querySelector("#data").innerHTML = konten;
 		});
 }
+
+requestJSON("");
+
+document.querySelector("#btnCari").addEventListener("click", function() {
+	var nama = document.querySelector("#inputNama").value;
+	requestJSON(nama);
+	console.log(nama);
+});
